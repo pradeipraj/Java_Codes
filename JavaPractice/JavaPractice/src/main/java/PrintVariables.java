c91a61319307a4ddce62b196e4f8db42ee3adf0d
PradeipRaj <pradeip009@gmail.com>
2023-07-21 14:18:27 +0530

    "PrintVariables" Class created.

---
+++ JavaPractice/JavaPractice/src/main/java/PrintVariables.java
@@ -0,0 +1,12 @@
+public class PrintVariables {
+    public static void main (String [] args)
+    {
+        String lastname = " Raj";
+        int x = 100;
+        double a = 1.9;
+
+        System.out.println("Pradeep" + lastname);
+        System.out.println("Hundred in Numbers is " + x);
+        System.out.println("One point nine in numbers is " + a);
+    }
+}
