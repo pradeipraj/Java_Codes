71a692bed5b56540761e6bb223f14e9c6606b42f
PradeipRaj <pradeip009@gmail.com>
2023-07-21 16:04:08 +0530

    Char Type class is created...

---
+++ JavaPractice/JavaPractice/src/main/java/CharType.java
@@ -0,0 +1,9 @@
+public class CharType {
+    public static void main (String[] args)
+    {
+        char myChar = 65;// Follow the table https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+        char a1 = 66;
+        System.out.println(myChar); // A will be printed.
+        System.out.println(a1); // B will be printed.
+    }
+}
