0a75949911de607b02825e9c3d34c3bde38d861a
PradeipRaj <pradeip009@gmail.com>
2023-07-21 14:28:54 +0530

    "MultiVariables" Class is created.

---
+++ JavaPractice/JavaPractice/src/main/java/MultiVariable.java
@@ -0,0 +1,20 @@
+public class MultiVariable {
+    static public void main(String[] args)
+    {
+        int x;
+        int y;
+        int z;
+        x = y = z = 50;
+        int a;
+        a = x*y;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(x+y);
+        System.out.println(y+z);
+        System.out.println(x+y+z);
+        System.out.println(a);
+
+    }
+}
