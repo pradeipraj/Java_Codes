dbf4e0d17ec008db4a6c2a732a45f07879992515
PradeipRaj <pradeip009@gmail.com>
2023-07-21 13:19:20 +0530

    "Variables" Class updated.

---
+++ JavaPractice/JavaPractice/src/main/java/Variables.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Variables {
+    public static void main (String[] args)
+    {
+        String firstname = "Pradeep";
+        String lastName = "Raj";
+        String dateOfBirth = "1990";
+        int age = 33;
+        String gender = "Male";
+
+        System.out.println(firstname);
+        System.out.println(lastName);
+        System.out.println(dateOfBirth);
+        System.out.println(age);
+        System.out.println(gender);
+    }
+}
