297e451a83d7ee0aee95002b6ee9037e66545372
PradeipRaj <pradeip009@gmail.com>
2023-07-28 11:35:51 +0530

    Main updated with PrintLn, Numbers and Comments

--- www/w3school/src/main/java/Main.java
+++ www/w3school/src/main/java/Main.java
@@ -3,6 +3,12 @@ public class Main {
     {
         System.out.println("Practicing Java Programming");
         System.out.print("Witout Next Line");
-        System.out.print("Passed");
+        System.out.println("Passed");
+        System.out.println(3);
+        System.out.println(100); // This is single line comment
+        System.out.println("Will See Comments");
+        /* Here we can use document comment so that
+        java will not execute these lines*/
+        System.out.println("This will Run and Executes");
     }
 }
