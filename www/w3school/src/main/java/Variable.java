364f817690c3181d04a707b187d31a8017402609
PradeipRaj <pradeip009@gmail.com>
2023-07-28 11:58:28 +0530

    Variable practice added

---
+++ www/w3school/src/main/java/Variable.java
@@ -0,0 +1,23 @@
+
+public class Variable {
+    public static void main (String [] args)
+    {
+        String name = "Pradeep";
+        int age = 30;
+        float result = 99.99f;
+        boolean x = true;
+        int a;
+        a = 120;
+        a = 130;
+
+        final int b;
+        b = 150;
+        System.out.println(b);
+        System.out.println(a);
+
+        System.out.println("The Name is " + name);
+        System.out.println("The age is " + age);
+        System.out.println("The Result is " + result);
+        System.out.println("The condition is " + x);
+    }
+}
