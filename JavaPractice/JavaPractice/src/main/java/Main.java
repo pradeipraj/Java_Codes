2e44ad3ab6df55d8364199ca4331e6bd86987ea1
PradeipRaj <pradeip009@gmail.com>
2023-07-28 11:15:29 +0530

    Main w3school java practicing

--- JavaPractice/JavaPractice/src/main/java/Main.java
+++ JavaPractice/JavaPractice/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("HEllo World");
+        System.out.println("Hello World");
         System.out.println("My First Java Program");
         System.out.println("First Program Done");
     }
---
+++ www/w3school/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String [] args)
+    {
+        System.out.println("Practicing Java Programming");
+        System.out.print("Witout Next Line");
+        System.out.print("Passed");
+    }
+}
