b00d14bf82e0d8148780d7f2cb166c1985ba73b7
PradeipRaj <pradeip009@gmail.com>
2023-07-28 12:31:01 +0530

    Multi Variable practice added

---
+++ www/w3school/src/main/java/MultiVariable.java
@@ -0,0 +1,20 @@
+public class MultiVariable {
+    public static void main (String[] args){
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        System.out.println("Addition: "+ (x+y+z));
+        System.out.println("Sub: " + (z-y));
+        System.out.println("Multiplication: " + (x*y*z));
+
+        int a;
+        int b;
+        int c;
+        a = b = c = 10;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}
