f517b69517e66b039041e096db3d6e344f1809a7
PradeipRaj <pradeip009@gmail.com>
2023-07-29 16:46:08 +0530

    JUnit Testing Adding parameters

--- Testing/JUnitTesting/src/test/java/CalculatorTest.java
+++ Testing/JUnitTesting/src/test/java/CalculatorTest.java
@@ -9,5 +9,14 @@ class CalculatorTest {
         int result = calculator.add(2,3);
         assertEquals(5,result);
     }
+    @Test
+    public void testAdd1(){
+        int param1 = 2;
+        int param2 = 3;
+        int expected = 5;
+        Calculator calculator = new Calculator();
+        int result = calculator.add(param1, param2);
+        assertEquals(expected, result);
+    }
 
 }
