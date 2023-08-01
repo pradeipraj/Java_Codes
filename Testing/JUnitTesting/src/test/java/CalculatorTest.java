047a12d9ac691625801e78bdd8c08633da1e2697
PradeipRaj <pradeip009@gmail.com>
2023-08-01 14:04:25 +0530

    Updated this.calculator instance BeforeEach and AfterEach annotations

--- Testing/JUnitTesting/src/test/java/CalculatorTest.java
+++ Testing/JUnitTesting/src/test/java/CalculatorTest.java
@@ -5,20 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
 
     @BeforeEach
     public void setup(){
         System.out.println("Setting up new Tests for Calculators.");
+        this.calculator = new Calculator();
     }
 
     @AfterEach
     public void teardown(){
         System.out.println("Tearing down after each tests done.");
+        this.calculator = null;
     }
 
     @Test
     public void testAdd(){
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator();
         int add = calculator.add(2,3);
         assertEquals(5, add);
     }
@@ -28,14 +31,14 @@ class CalculatorTest {
         int a = 2000;
         int b = 3000;
         int expected = 5000;
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator();
         int result = calculator.add(a, b);
         assertEquals(expected, result);
     }
 
     @Test
     public void testSub(){
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         int sub = calculator.sub(2,1);
         assertEquals(1, sub);
     }
@@ -45,7 +48,7 @@ class CalculatorTest {
         int a = 300;
         int b = 200;
         int expected = 100;
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator();
         int result = calculator.sub(a,b);
         assertEquals(expected, result);
 
@@ -53,7 +56,7 @@ class CalculatorTest {
 
     @Test
     public void testMul(){
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator();
         int mul = calculator.mul(10, 20);
         assertEquals(200, mul);
     }
@@ -63,14 +66,14 @@ class CalculatorTest {
         int a = 10;
         int b = 20;
         int expected = 200;
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         int result = calculator.mul(a, b);
         assertEquals(expected, result);
     }
 
     @Test
     public void testDiv(){
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         double div = calculator.div(20, 2);
         assertEquals(10, div);
     }
@@ -80,7 +83,7 @@ class CalculatorTest {
         double a = 100;
         double b = 20;
         double expected = 5;
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         double result = calculator.div(a, b);
         assertEquals(expected, result);
     }
@@ -90,14 +93,14 @@ class CalculatorTest {
         double a = 10;
         double b = 2.5;
         double expected = 4;
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         double result = calculator.div(a, b);
         assertEquals(expected, result);
     }
 
     @Test
     public void testPer(){
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         double per = calculator.per(450, 600, 100);
         assertEquals(75, per);
     }
@@ -107,21 +110,21 @@ class CalculatorTest {
         double obtainedMarks = 450;
         double totalMarks = 600;
         double expected = 75;
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         double result = calculator.per(obtainedMarks, totalMarks, expected);
         assertEquals(expected, result);
     }
 
     @Test
     public void testIsEven(){
-        Calculator calculator = new Calculator();
+     //   Calculator calculator = new Calculator();
         assertTrue(calculator.isEven(2));
         assertFalse(calculator.isEven(3));
     }
 
     @Test
     public void testAddToArray(){
-        Calculator calculator = new Calculator();
+      //  Calculator calculator = new Calculator();
         int[] array = {1,2,3};
         int numToAdd = 6;
         int[] result = calculator.addToArray(array, numToAdd);
