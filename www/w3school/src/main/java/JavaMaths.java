d1d89774c8c7364b27c683b33c11ddfc1a7e010b
PradeipRaj <pradeip009@gmail.com>
2023-08-04 20:12:33 +0530

    Java Math Added

---
+++ www/w3school/src/main/java/JavaMaths.java
@@ -0,0 +1,13 @@
+public class JavaMaths {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 12;
+
+        int maxNum = Math.max(x,y);
+        int minNum = Math.min(x,y);
+
+        System.out.println(maxNum);
+        System.out.println(minNum);
+    }
+}
