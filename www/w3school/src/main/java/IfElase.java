17e4b74f98d78bcc6544d74de29d52b78e420a69
PradeipRaj <pradeip009@gmail.com>
2023-08-05 13:38:20 +0530

    Java If Else added

---
+++ www/w3school/src/main/java/IfElase.java
@@ -0,0 +1,31 @@
+public class IfElase {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        if (x > y) {
+            System.out.println("x is largest number");
+        } else {
+            System.out.println("y is largest number");
+        }
+
+        int batPrice = 800 ;
+        if (batPrice == 200){
+            System.out.println("80% Discount");
+        } else if (batPrice == 500){
+            System.out.println("50% Discount");
+        } else if (batPrice == 800){
+            System.out.println("20% Discount");
+        } else if (batPrice == 1000){
+            System.out.println("No Discount");
+        } else if (batPrice > 1000){
+            System.out.println("Too costly.");
+        }
+
+        String result = (batPrice >= 1000) ? "No Discount" : "Some Discount";
+        System.out.println(result);
+
+    }
+}
