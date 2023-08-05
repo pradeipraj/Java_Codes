793aaaf361e6f4818e31486769fc05e9cc5514eb
PradeipRaj <pradeip009@gmail.com>
2023-08-05 12:58:04 +0530

    Java Maths operations

--- www/w3school/src/main/java/JavaMaths.java
+++ www/w3school/src/main/java/JavaMaths.java
@@ -3,11 +3,22 @@ public class JavaMaths {
 
         int x = 10;
         int y = 12;
+        double z = -2.5;
 
         int maxNum = Math.max(x,y);
         int minNum = Math.min(x,y);
+        double squareRoot = Math.sqrt(x);
+        double absValue = Math.abs(z);
 
         System.out.println(maxNum);
         System.out.println(minNum);
+        System.out.println(squareRoot);
+        System.out.println(absValue);
+
+        //Random number between 0.0 to 1.0
+        System.out.println(Math.random());
+        //Random number between 1 and 100
+        System.out.println(Math.random()*1000);
+
     }
 }
