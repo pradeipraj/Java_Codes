d3d6bc12bed36457bc8cc60d5d19b3e37c559b57
PradeipRaj <pradeip009@gmail.com>
2023-08-06 16:02:29 +0530

    Java Two Dimensional Array

---
+++ www/w3school/src/main/java/TwoDimensionArray.java
@@ -0,0 +1,8 @@
+public class TwoDimensionArray {
+    public static void main(String[] args) {
+
+        int [] [] myNumbers = {{1,2,3,4}, {5,6,7,8}};
+        myNumbers [0][3] = 10;
+        System.out.println(myNumbers[0][3]);
+    }
+}
