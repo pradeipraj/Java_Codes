5e6a36415ae6ea864be087b39d5cb1e01623b023
PradeipRaj <pradeip009@gmail.com>
2023-08-06 14:37:55 +0530

    Java Loops

---
+++ www/w3school/src/main/java/JavaLoops.java
@@ -0,0 +1,28 @@
+public class JavaLoops {
+    public static void main(String[] args) {
+
+        //While Loop
+        int i = 0;
+        while (i <= 5){
+            System.out.println(i);
+            i++;
+        }
+
+        //Do While Loop
+        int x = 0;
+        do {
+            System.out.println(x);
+            x++;
+        }while (x<5);
+
+        int y;
+        for (y = 0; y < 5; y++){
+            System.out.println(y);
+        }
+
+        int z;
+        for (z=0; z<=20; z=z+2){
+            System.out.println(z);
+        }
+    }
+}
