ba476083f61ef2d296ee155e81b47277b7845aa3
PradeipRaj <pradeip009@gmail.com>
2023-08-08 10:01:29 +0530

    Java Methods...

---
+++ www/w3school/src/main/java/JavaMethods.java
@@ -0,0 +1,18 @@
+public class JavaMethods {
+
+    static void myMethod(){
+        System.out.println("This is my method");
+    }
+
+    static void myMethod1(){
+        System.out.println("This is method1");
+    }
+    static void myMethod2(){
+        System.out.println("This is method2");
+    }
+    public static void main(String[] args) {
+        myMethod();
+        myMethod1();
+        myMethod2();
+    }
+}
