3b0c2a3038b83140248bc4b522303f4faf2d2b50
PradeipRaj <pradeip009@gmail.com>
2023-08-08 15:17:04 +0530

    Java Recursion

---
+++ www/w3school/src/main/java/JavaRecursion.java
@@ -0,0 +1,12 @@
+public class JavaRecursion {
+    public static void main(String[] args) {
+        int result = sum(100);
+        System.out.println(result);
+    }
+
+    public static int sum (int k){
+        if (k>0){
+            return k + sum(k-1);
+        }else return 0;
+    }
+}
