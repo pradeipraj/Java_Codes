2c978d8aa407964d5a4739ccfb6fff8f7af12907
Pradeep Raj <129373542+pradeipraj@users.noreply.github.com>
2023-08-09 16:23:57 +0530

    BLM Added
    Book Lending Management Project Added

---
+++ blm/src/main/java/com/softwiz/blm/BlmApplication.java
@@ -0,0 +1,13 @@
+package com.softwiz.blm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlmApplication.class, args);
+	}
+
+}
---
+++ blm/src/test/java/com/softwiz/blm/BlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwiz.blm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
