16903978f1a57574e77b14d0c7f299409c39a587
PradeipRaj <pradeip009@gmail.com>
2023-08-09 16:26:32 +0530

    Book Lending Management project (Spring Boot Application)

---
+++ src/main/java/com/softwiz/blm/BlmApplication.java
@@ -0,0 +1,14 @@
+package com.softwiz.blm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlmApplication.class, args);
+	}
+
+	//This is the main of the spring project
+}
